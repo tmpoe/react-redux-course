f9f8cdc2b218a10426f15df1325cef785093fd6d
tmpoe <torok.mark.patrik@gmail.com>
2024-04-11 15:03:34 +0200

    init test proj

---
+++ react_essentials_project/react_essentials_project/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import reactLogo from '../../assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export function Header() {
+    return (
+    <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+            Fundamental React concepts you will need for almost any app you are
+            going to build!
+        </p>
+    </header>
+  )
+}
